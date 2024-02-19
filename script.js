8200cefaf3813aa90832ea901ab8e74f3b5f0a77
Aneesh <as416106@gmail.com>
2024-02-19 22:58:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const video = document.getElementById("video");
 async function startCamera() {
   try {
     // Request permission to access the camera
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
     // Set the video source to the camera stream
     video.srcObject = stream;
   } catch (error) {
